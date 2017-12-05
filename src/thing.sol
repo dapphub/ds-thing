4c86a534b2cdaf7c7a8564dfd8572ef466615a00
Andy Milenius <andrew.milenius@gmail.com>
2017-12-05 16:23:43 -0500

    add S function for generating signatures

--- src/thing.sol
+++ src/thing.sol
@@ -22,4 +22,9 @@ import 'ds-note/note.sol';
 import 'ds-math/math.sol';
 
 contract DSThing is DSAuth, DSNote, DSMath {
+
+    function S(string s) internal pure returns (bytes4) {
+        return bytes4(keccak256(s));
+    }
+
 }
